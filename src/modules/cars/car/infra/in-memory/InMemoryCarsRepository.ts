803981f225437e13d77147ce4db3f12ef08ce589
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-04-04 18:47:58 -0300

    test: create in memory car repository for testing

---
+++ src/modules/cars/car/infra/in-memory/InMemoryCarsRepository.ts
@@ -0,0 +1,21 @@
+import { CarDto } from "../../dto/CarDto";
+import { Car } from "../../model/Car";
+import { ICarsRepository } from "../../repositories/ICarsRepository";
+
+class InMemoryCarsRepository implements ICarsRepository {
+  private cars: Car[];
+
+  create(data: CarDto): Car {
+    const car = new Car();
+
+    Object.assign(car, {
+      ...data,
+    });
+
+    this.cars.push(car);
+
+    return car;
+  }
+}
+
+export { InMemoryCarsRepository };
