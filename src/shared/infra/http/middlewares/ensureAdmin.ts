bb588ea6a73dd6c5a2077a0e1d39657e17283902
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-04-04 18:09:31 -0300

    feat: create middleware to verify if user is admin

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../../../../errors/AppError";
+
+function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const { user } = request;
+
+  if (!user.isAdmin) {
+    throw new AppError("Operation not authorized.");
+  }
+
+  next();
+}
+
+export { ensureAdmin };
--- src/shared/infra/http/routes/category.routes.ts
+++ src/shared/infra/http/routes/category.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import multer from "multer";
-import { CreateCategoryController } from "../../../../modules/category/useCases/createCategory/CreateCategoryController";
-import { ImportCategoryController } from "../../../../modules/category/useCases/importCategory/ImportCategoryController";
-import { ListCategoryController } from "../../../../modules/category/useCases/listCategory/ListCategoriesController";
+import { CreateCategoryController } from "../../../../modules/cars/category/useCases/createCategory/CreateCategoryController";
+import { ImportCategoryController } from "../../../../modules/cars/category/useCases/importCategory/ImportCategoryController";
+import { ListCategoryController } from "../../../../modules/cars/category/useCases/listCategory/ListCategoriesController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoryRoutes = Router();
@@ -13,6 +14,8 @@ const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoryController();
 
 categoryRoutes.use(ensureAuthenticated);
+
+categoryRoutes.use(ensureAdmin);
 
 categoryRoutes.post("/", createCategoryController.handle);
 
