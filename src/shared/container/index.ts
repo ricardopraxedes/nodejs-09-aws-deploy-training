1f01370193b2a32b9668f53e1cabb1f4e8f49be3
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-04-04 18:13:57 -0300

    refactor: move category to cars module

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,6 @@
 import { container } from "tsyringe";
-import { CategoriesRepository } from "../../modules/category/infra/typeorm/repository/CategoriesRepository";
-import { ICategoriesRepository } from "../../modules/category/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/category/infra/typeorm/repository/CategoriesRepository";
+import { ICategoriesRepository } from "../../modules/cars/category/repositories/ICategoriesRepository";
 import { UsersRepository } from "../../modules/users/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "../../modules/users/repositories/IUsersRepository";
 
