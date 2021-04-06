fdc27c95932481adf8c71a076000cc995978b41d
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-04-06 15:08:43 -0300

    add controller and routes to list available cars use case

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateCarController } from "../../../../modules/cars/car/useCases/CreateCar/CreateCarController";
+import { ListAvailableCarsController } from "../../../../modules/cars/car/useCases/ListAvailableCars/ListAvailableCarsController";
 
 const carRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carRoutes.post("/", createCarController.handle);
+
+carRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carRoutes };
