8905e2499b0d4a53a2cc49317426676e98199796
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-05-06 08:58:46 -0300

    add cors

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 import { AppError } from "@shared/errors/AppError";
+import cors from "cors";
 import swaggerFile from "../../../swagger.json";
 import "../../container";
 import { createDBConnection } from "../typeorm";
@@ -14,6 +15,7 @@ createDBConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
